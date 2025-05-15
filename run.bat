7cf4c6f7f9e13d97479b4fd61cd3c13b1b6b8032
jmpcps <jmpcps@gmail.com>
2025-05-15 19:06:10 +0100

    run atualizado

--- run.bat
+++ run.bat
@@ -1,7 +1,17 @@
 @echo off
 chcp 65001 > nul
 echo Compilando jao.cpp com g++...
+
 g++ -std=c++14 jao.cpp -o jao.exe
 if %errorlevel% neq 0 (
     echo Houve um erro na compilação.
     pause
+    exit /b
+)
+
+echo Compilação bem-sucedida. A executar...
+echo.
+jao.exe
+
+echo.
+pause
